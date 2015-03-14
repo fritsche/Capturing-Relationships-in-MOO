81129e15fff4636f7db3aa3485ba291cdb29aeb6
Gian Mauricio Fritsche <gian.fritsche@gmail.com>
2015-03-14 17:17:47 -0300

    Testing Apache Commons Math...

--- jmetal4.5.src/jmetal/learning/ApacheCommonsMathTest.java
+++ jmetal4.5.src/jmetal/learning/ApacheCommonsMathTest.java
@@ -2,10 +2,57 @@
 
 package jmetal.learning;
 
+import org.apache.commons.math3.stat.correlation.Covariance;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.apache.commons.math3.stat.correlation.SpearmansCorrelation;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
+
 public class ApacheCommonsMathTest {
 
 	public ApacheCommonsMathTest(){
-		System.out.println("Testing Apache Commons Math...");
+		System.out.println("Testing Apache Commons Math...\n");
+
+		System.out.println("Input data:");
+		double[][] data = { {1.0, 10.0, 5.0, 3.0},
+							{2.0, 20.0, 4.0, 3.0},
+							{3.0, 30.0, 3.0, 3.0},
+							{4.0, 40.0, 2.0, 3.0},
+							{5.0, 50.0, 1.0, 3.0}};
+		printMatrix(data);
+		System.out.println();
+
+		System.out.println("Covariance:");
+		Covariance covariance = new Covariance(data);
+		RealMatrix matrix = covariance.getCovarianceMatrix();
+		double[][] result = matrix.getData();
+		printMatrix(result);
+		System.out.println();
+
+		System.out.println("Pearsons Correlation:");
+		double[][] pearsons = (new PearsonsCorrelation(data)).getCorrelationMatrix().getData();
+		printMatrix(pearsons);
+		System.out.println();
+
+		System.out.println("Spearmans Correlation:");
+		double[][] spearmans = (new SpearmansCorrelation()).computeCorrelationMatrix(data).getData();
+		printMatrix(spearmans);
+		System.out.println();
+
+		System.out.println("Kendalls Correlation:");
+		double[][] kendalls = (new KendallsCorrelation(data)).getCorrelationMatrix().getData();
+		printMatrix(kendalls);
+		System.out.println();
+
+	}
+
+	protected void printMatrix(double[][] matrix){
+		for (double[] i : matrix) {
+			for (double j : i) {
+				System.out.print("\t" + j);
+			}
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) {
--- jmetal4.5.src/script.sh
+++ jmetal4.5.src/script.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 reset
+echo "fixing non ASCII chars on jmetal..."
+export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 
-#compiling
-find -name "*.java" > sources.txt
-javac @sources.txt
-rm sources.txt
-#running
-#java jmetal.metaheuristics.singleObjective.cmaes.CMAES_main > output.txt
+echo "loading java files..."
+find -name "*.java" > sources.aux
+echo "compiling..."
+javac -classpath ".:libs/commons-math3-3.4.1/commons-math3-3.4.1.jar" @sources.aux
+#if error
+if ! [ $? -eq 0 ]; then
+	echo "COMPILATION ERROR!"
+else
+	echo "removing aux files..."
+	rm *.aux
+	echo "running..."
+	#java jmetal.metaheuristics.singleObjective.cmaes.CMAES_main > output.txt
+	java -classpath ".:libs/commons-math3-3.4.1/commons-math3-3.4.1.jar" jmetal.learning.ApacheCommonsMathTest
+fi
+
+echo "done."
