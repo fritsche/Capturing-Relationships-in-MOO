86ddc3bceb8e5ecd8116017f9874ea3d9b94a60d
Gian Mauricio Fritsche <gian.fritsche@gmail.com>
2015-03-14 15:35:28 -0300

    removed weka; added apache commons math

---
+++ jmetal4.5.src/jmetal/learning/ApacheCommonsMathTest.java
@@ -0,0 +1,13 @@
+/* apache commons math test */
+
+public class ApacheCommonsMathTest {
+
+	public ApacheCommonsMathTest(){
+		System.out.println("Testing Apache Commons Math...");
+	}
+
+	public static void main(String[] args) {
+		new ApacheCommonsMathTest();
+	}
+
+}
