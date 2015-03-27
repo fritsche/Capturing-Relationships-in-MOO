a759861e47359cad14602c03d99eea33a0be2fa6
Andrei Strickler <stricklerandrei@gmail.com>
2015-03-27 15:28:01 -0300

    update Visualization

--- jmetal4.5.src/jmetal/visualization/Visualization.java
+++ jmetal4.5.src/jmetal/visualization/Visualization.java
@@ -42,10 +42,12 @@ public class Visualization extends JFrame {
         styleV.put(mxConstants.STYLE_FILLCOLOR, "#00FF00");
         styleV.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
         styleV.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
+        styleV.put(mxConstants.STYLE_FONTCOLOR, "#000000");
         stylesheet.putCellStyle("variable", styleV);
 
         styleO.put(mxConstants.STYLE_FILLCOLOR, "#FFFF00");
         styleO.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        styleO.put(mxConstants.STYLE_FONTCOLOR, "#000000");
         styleO.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
         stylesheet.putCellStyle("objective", styleO);
         
@@ -130,8 +132,9 @@ public class Visualization extends JFrame {
 		}else{
 			style.put(mxConstants.STYLE_STROKECOLOR, "#FF0000");
 		}
-		style.put(mxConstants.STYLE_STROKEWIDTH, Math.abs(value)*20);
+		style.put(mxConstants.STYLE_STROKEWIDTH, Math.abs(value)*10);
         style.put(mxConstants.STYLE_EDGE,mxConstants.SHAPE_CONNECTOR);
+        style.put(mxConstants.STYLE_FONTCOLOR,"#000000");
         stylesheet.putCellStyle(Double.toString(value), style);
         graph.insertEdge(parent, null, String.valueOf(value).subSequence(0, 5),nodes.get(startNode), nodes.get(endNode),Double.toString(value));
 	}
